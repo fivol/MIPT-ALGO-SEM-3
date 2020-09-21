4d43909d0056fb35246f18d2dfe512d2bb699777
Boris <boris2000n@gmail.com>
2020-09-21 14:00:12 +0300

    fix brackets

--- sem3_task1_b_palindroms/solution.cpp
+++ sem3_task1_b_palindroms/solution.cpp
@@ -5,21 +5,26 @@
 int64_t palindromeSubstringsAmount(const std::string &text) {
     size_t leftBorderOdd = 0, rightBorderOdd = 0;
     size_t leftBorderEven = 0, rightBorderEven = 0;
-    std::vector<size_t> palindromeSizeOdd(text.size(), 0);
-    std::vector<size_t> palindromeSizeEven(text.size(), 0);
+    std::vector<size_t> palindromeSizeOdd(text.size());
+    std::vector<size_t> palindromeSizeEven(text.size());
     for (size_t i = 0; i < text.size(); ++i) {
-        if (i < rightBorderOdd)
+        if (i < rightBorderOdd){
             palindromeSizeOdd[i] = std::min(rightBorderOdd - i, palindromeSizeOdd[leftBorderOdd + rightBorderOdd - i]);
-        if (i < rightBorderEven)
+
+        }
+        if (i < rightBorderEven){
             palindromeSizeEven[i] = std::min(rightBorderEven - i, palindromeSizeEven[leftBorderEven + rightBorderEven - i]);
 
+        }
         while (i + palindromeSizeOdd[i] < text.size() && i - palindromeSizeOdd[i] >= 0 &&
-               text[i + palindromeSizeOdd[i]] == text[i - palindromeSizeOdd[i]])
+               text[i + palindromeSizeOdd[i]] == text[i - palindromeSizeOdd[i]]){
             ++palindromeSizeOdd[i];
+        }
         while (i + palindromeSizeEven[i] < text.size() && i - palindromeSizeEven[i] - 1 >= 0 &&
-               text[i + palindromeSizeEven[i]] == text[i - palindromeSizeEven[i] - 1])
+               text[i + palindromeSizeEven[i]] == text[i - palindromeSizeEven[i] - 1]){
             ++palindromeSizeEven[i];
-        
+        }
+
         if (i + palindromeSizeOdd[i] > rightBorderOdd) {
             rightBorderOdd = i + palindromeSizeOdd[i];
             leftBorderOdd = i - palindromeSizeOdd[i];
@@ -32,11 +37,13 @@ int64_t palindromeSubstringsAmount(const std::string &text) {
     }
 
     int64_t amount = 0;
-    for (auto count: palindromeSizeOdd)
+    for (auto count: palindromeSizeOdd){
         amount += count - 1;
-    
-    for (auto count: palindromeSizeEven)
+    }
+
+    for (auto count: palindromeSizeEven){
         amount += count;
+    }
 
     return amount;
 }
